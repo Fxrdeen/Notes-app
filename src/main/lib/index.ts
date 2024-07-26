e67b464c6ec53d74fca0e4e1acb63546f6488f7f
Fardeen <fardeenclan@gmail.com>
2024-07-26 20:53:24 +0530

    added create and delete and final

--- src/main/lib/index.ts
+++ src/main/lib/index.ts
@@ -1,10 +1,12 @@
 import { appDirectoryName, fileEncoding } from '@shared/constants'
 import { NoteInfo } from '@shared/models'
-import { GetNotes, ReadNote, WriteNote } from '@shared/types'
-import { readFile, stat, writeFile } from 'fs-extra'
+import { CreateNote, DeleteNote, GetNotes, ReadNote, WriteNote } from '@shared/types'
+import { dialog } from 'electron'
+import { readFile, remove, stat, writeFile, writeFileSync } from 'fs-extra'
 import { ensureDir, readdir } from 'fs-extra'
 import { homedir } from 'os'
-
+import path from 'path'
+import welcomeFile from '../../../resources/welcomeNote.md?asset'
 export const getRootDir = () => {
   return `${homedir()}/${appDirectoryName}`
 }
@@ -17,6 +19,12 @@ export const getNotes: GetNotes = async () => {
     withFileTypes: false
   })
   const notes = notesFileNames.filter((filename) => filename.endsWith('.md'))
+  if (notes.length === 0) {
+    console.info('No notes found, creating welcome note')
+    const content = await readFile(welcomeFile, { encoding: fileEncoding })
+    await writeFile(`${root}/welcome.md`, content, { encoding: fileEncoding })
+    notes.push('welcome.md')
+  }
   return Promise.all(notes.map(getNoteInfoFromFilename))
 }
 export const getNoteInfoFromFilename = async (filename: string): Promise<NoteInfo> => {
@@ -34,4 +42,52 @@ export const writeNote: WriteNote = async (filename, content) => {
   const rootDir = getRootDir()
   console.info(`Writing note ${filename}}`)
   return writeFile(`${rootDir}/${filename}.md`, content, { encoding: fileEncoding })
+}
+export const createNote: CreateNote = async () => {
+  const rootDir = getRootDir()
+  await ensureDir(rootDir)
+  const { filePath, canceled } = await dialog.showSaveDialog({
+    title: 'New note',
+    defaultPath: `${rootDir}/Untitled.md`,
+    buttonLabel: 'Create',
+    properties: ['showOverwriteConfirmation'],
+    showsTagField: false,
+    filters: [{ name: 'Markdown', extensions: ['md'] }]
+  })
+  if (canceled || !filePath) {
+    console.info('Note creation cancelled')
+    return false
+  }
+  const { name: filename, dir: parentDir } = path.parse(filePath)
+  if (parentDir !== rootDir) {
+    await dialog.showMessageBox({
+      type: 'error',
+      title: 'Creation Failed',
+      message: `All notes must be saved under ${rootDir}. Avoid using other directories
+      `
+    })
+    return false
+  }
+  console.log(`Creating note: ${filePath}`)
+  await writeFileSync(filePath, '')
+  return filename
+}
+
+export const deleteNote: DeleteNote = async (filename) => {
+  const rootDir = getRootDir()
+  const { response } = await dialog.showMessageBox({
+    type: 'warning',
+    title: 'Delete Note',
+    message: `are you sure you want to delete ${filename}?`,
+    buttons: ['Delete', 'Cancel'],
+    defaultId: 1,
+    cancelId: 1
+  })
+  if (response === 1) {
+    console.info('note deletion cancelled')
+    return false
+  }
+  console.log(`Deleting note ${filename}`)
+  await remove(`${rootDir}/${filename}.md`)
+  return true
 }
